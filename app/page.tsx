165fe0a2e929e56d015feeef274311066849e800
Jarin Tasnim Eara <jarintasnimeara@gmail.com>
2026-07-03 01:11:22 +0600

    Load book data from local JSON instead of localhost

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
 import Hero from '../components/Hero';
 import BookGrid from '../components/BookGrid';
+import books from '../data/books.json';
 
 
-async function getBooks() {
-  const res= await fetch('http://localhost:3000/books.json', {
-    
-  });
-  return res.json();
-}
-
-export default async function Home() {
-  const books= await getBooks();
+export default function Home() {
 
   return (
     <main className="bg-white min-h-screen">
